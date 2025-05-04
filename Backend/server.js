d3413a47961e04956e3af50836be7d1e11e75c75
jackcodes64 <jackokebe64@gmail.com>
2025-05-04 22:29:11 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:5173',        
-  'cm-dashboard-xbo3.vercel.app',      
+  'https://cm-dashboard-xbo3.vercel.app'',      
 ];
 
 app.use(cors({
