c80588460e2f483f804a5c2b2353dd637a24e38d
jackcodes64 <jackokebe64@gmail.com>
2025-05-04 18:57:05 +0300

    Create database.js

---
+++ Backend/utils/database.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+// Setting up a MySQL connection using environment variables
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
+
+module.exports = sequelize;
