07bf3e417018a022a742f79eb1572142992e8108
jackcodes64 <jackokebe64@gmail.com>
2025-05-04 22:37:10 +0300

    Update user.js

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,9 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true // optional, but usually needed for ID fields
+    },
     username: { 
       type: DataTypes.STRING, 
-      allowNull: false, 
+      allowNull: false
     },
-    password: { type: DataTypes.STRING, allowNull: false }
-  } );
+    password: { 
+      type: DataTypes.STRING, 
+      allowNull: false
+    }
+  });
 };
