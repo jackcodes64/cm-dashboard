1c1bfb992f07a6fd47b3edf0274758305efc8dda
jackcodes64 <jackokebe64@gmail.com>
2025-05-04 22:09:00 +0300

    Update index.js

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 // Setup connection
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
   logging: false,
 });
 
